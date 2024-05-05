c673bcc1e189eb42027da510ea4b0c43e7395acc
J. Cox <Cox00233@umn.edu>
2024-05-05 13:31:28 -0500

    Adds start of time derivatives and midoint function

---
+++ timeDerivatives.m
@@ -0,0 +1,5 @@
+%% Time Derivatives
+%  Computes the first time derivatives for the average trajectory in case B
+%  using midpoint method and plots them in a 2x1 grid of subplots
+
+dHeight = num_der_central
